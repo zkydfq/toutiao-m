63ad0fd5223c3172090d8dcf821641f7eb5ba732
zkydfq <2052889673@qq.com>
2021-08-09 19:36:09 +0800

    项目初始化-引入vant组件库

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import store from './store'
 import './styles/index.less'
 
+// 加载注册 Vant 组件
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
